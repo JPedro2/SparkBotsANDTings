3758b185798512bed7007567659fc6d7fa9d1b01
Pedro Oliveira <peolivei@cisco.com>
2017-04-20 16:37:35 +0100

    integration with ngrok

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ var controller = Botkit.sparkbot({
     ciscospark_access_token: process.env.access_token,
     studio_token: process.env.studio_token, // get one from studio.botkit.ai to enable content management, stats, message console and more
     secret: process.env.secret, // this is an RECOMMENDED but optional setting that enables validation of incoming webhooks
-    webhook_name: 'Cisco Spark bot created with Botkit, override me before going to production',
+    webhook_name: 'Casey Webhook',
     studio_command_uri: process.env.studio_command_uri,
 });
 
@@ -98,7 +98,7 @@ function usage_tip() {
     console.log('~~~~~~~~~~');
     console.log('Botkit Studio Starter Kit');
     console.log('Execute your bot application like this:');
-    console.log('access_token=<MWM4NGQ4MGYtMDQwMy00NWE0LThlODQtZGU0NTQwZDFkYTYyZTc3ZWZiYTgtZjhj> public_address=<https://mybotapp/> node bot.js');
+    console.log('access_token=MDhkYWIzYWMtMDYwOC00MWU0LTg0MjAtODljYTYwMTgzNGViM2M5NTA0YTUtMDVl public_address=https://3aa74f84.ngrok.io node bot.js');
     console.log('Get Cisco Spark token here: https://developer.ciscospark.com/apps.html')
     console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
     console.log('~~~~~~~~~~');
