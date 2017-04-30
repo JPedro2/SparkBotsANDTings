c3f689a835112f83dd99ab177cc42d8ab6e2d5de
Pedro Oliveira <peolivei@cisco.com>
2017-04-30 20:05:10 +0100

    Major Changes: Casey is now able to ask clarification questions and send an email out to the GVE case handling team

--- skills/Adding_Casey_To_a_Space.js
+++ skills/Adding_Casey_To_a_Space.js
@@ -1,17 +1,23 @@
 module.exports = function(controller) {
 
+  controller.on('bot_space_join', function(bot, message) {
 
+    bot.reply(message, 'Hello, ' + message.original_message.data.personDisplayName + ' my name is Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
+
+  });
+};
+
+/*
   controller.on('bot_space_join', function(bot, message) {
 
     bot.reply(message, 'Hi, I am a Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
 
   });
 
+
   controller.on('user_space_join', function(bot, message) {
 
     bot.reply(message, 'Hello, ' + message.original_message.data.personDisplayName + 'my name is Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
 
   });
-
-
-};
+*/
