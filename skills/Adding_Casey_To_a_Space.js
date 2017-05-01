2ba07803f938a02d033717c0d9e6030a2c849b40
Pedro Oliveira <peolivei@cisco.com>
2017-05-01 23:59:25 +0100

    Added date validation capabilities and different workflows for deal ID scenario

--- skills/Adding_Casey_To_a_Space.js
+++ skills/Adding_Casey_To_a_Space.js
@@ -2,22 +2,17 @@ module.exports = function(controller) {
 
   controller.on('bot_space_join', function(bot, message) {
 
-    bot.reply(message, 'Hello, ' + message.original_message.data.personDisplayName + ' my name is Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
-
-  });
-};
-
-/*
-  controller.on('bot_space_join', function(bot, message) {
-
-    bot.reply(message, 'Hi, I am a Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
+    bot.reply(message, 'Hi, my name is Casey 🤖 and I will be helping you raising Cases with GVE.       \n'
+                        + 'How may I help you?');
 
   });
 
+  controller.on('user_room_join', function(bot, message) {
 
-  controller.on('user_space_join', function(bot, message) {
-
-    bot.reply(message, 'Hello, ' + message.original_message.data.personDisplayName + 'my name is Casey and I will be helping you raising Cases with GVE. \b How may I help you?');
-
+      bot.reply(message, 'Hi, my name is Casey 🤖 and I will be helping you raising Cases with GVE.       \n'
+                          + 'How may I help you?');
   });
+/* user_room_join
+
 */
+};
